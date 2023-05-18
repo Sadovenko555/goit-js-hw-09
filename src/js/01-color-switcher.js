024c91a822d48c750845a5bb102d24371d6625a7
Ihor <sadovenko555@gmail.com>
2023-05-18 20:40:02 +0300

    add script for task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', startColorSwitch);
+stopButton.addEventListener('click', stopColorSwitch);
+
+function startColorSwitch() {
+  startButton.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopColorSwitch() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+  const body = document.body;
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
